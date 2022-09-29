9fd92fccce1443a4552acea1f07c965dd9c971b8
Игорь Медведев <getmetut@bk.ru>
2022-09-29 17:58:43 +0400

    микро коммит

---
+++ ChatAnalyzer/PersonalText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAnalyzer
+{
+    internal class PersonalText
+    {
+        PersonalText()
+        {
+            string text1, text2;
+
+            while (ChatInfo.Text.IndexOf()
+        }
+    }
+}
