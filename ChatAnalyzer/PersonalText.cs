23124d5167e3385e9606eaa6c9a14bf5dbeb3269
Игорь Медведев <getmetut@bk.ru>
2022-10-03 18:23:33 +0400

    ипсправил парсер по персоне, но не до  конца

--- ChatAnalyzer/PersonalText.cs
+++ ChatAnalyzer/PersonalText.cs
@@ -15,32 +15,42 @@ namespace ChatAnalyzer
             string person1 = ChatInfo.Person1, person2 = ChatInfo.Person2;
             int counter1 = 0, counter2 = 0;
             int index1 = text.IndexOf(person1, counter1), index2 = text.IndexOf(person2, counter2);
-            bool indexFlag1 = Char.IsDigit(text[index1 + 2]), indexFlag2 = Char.IsDigit(text[index2 + 2]);
+            bool indexFlag1 = Char.IsDigit(text[index1 + person1.Length + 1]), indexFlag2 = Char.IsDigit(text[index2 + person2.Length + 1]);
 
             if (!File.Exists("temp\\text1.txt") || !File.Exists("temp\\text2.txt"))
             {
                 while ( index1 != -1 || index2 != -1 )
                 {
                     if (indexFlag1 && indexFlag2)
-                        if ( index1 < index2 )
+                    {
+                        if (index1 < index2)
                         {
-                            text1.Append(text.Substring(index1, index2).Trim());
-                            counter1 = index2 + person2.Length;
+                            text1.Append(text.Substring(index1, index2 - index1 - 1).Trim()).Append(" ");
+                            counter1 = index1 + person1.Length;
+                            index1 = text.IndexOf(person1, counter1);
+                            indexFlag1 = Char.IsDigit(text[index1 + person1.Length + 1]);
                         }
                         else
                         {
-                            text1.Append(text.Substring(index2, index1).Trim());
-                            counter2 = index1 + person1.Length;
+                            text2.Append(text.Substring(index2, index1 - index2 - 1).Trim()).Append(" ");
+                            counter2 = index2 + person2.Length;
+                            index2 = text.IndexOf(person2, counter2);
+                            indexFlag2 = Char.IsDigit(text[index2 + person2.Length + 1]);
                         }
+                    }
                     else
                     {
-                       if (indexFlag1)
+                        if (indexFlag1)
                         {
-                            counter2 = index1 + person1.Length;
+                            counter2 = index2 + person2.Length;
+                            index2 = text.IndexOf(person2, counter2);
+                            indexFlag2 = Char.IsDigit(text[index2 + person2.Length + 1]);
                         }
-                       else
+                        else
                         {
-                            counter1 = index2 + person2.Length;
+                            counter1 = index1 + person1.Length;
+                            index1 = text.IndexOf(person1, counter1);
+                            indexFlag1 = Char.IsDigit(text[index1 + person1.Length + 1]);
                         }
                     }
                 }
